519fa6eec292a1e09001f32254a356e3953317bd
jaupventur <jaupventur@gmail.com>
2025-09-17 20:24:13 -0300

    fix(VTK-12): Retirando EnableJPAauditing

--- cimob/src/main/java/fatec/vortek/cimob/CimobApplication.java
+++ cimob/src/main/java/fatec/vortek/cimob/CimobApplication.java
@@ -2,10 +2,8 @@ package fatec.vortek.cimob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class CimobApplication {
 
     public static void main(String[] args) {
